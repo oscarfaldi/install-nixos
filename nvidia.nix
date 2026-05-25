902073a5059020d70fade3c70dd1891127fb8a6f
Oscar Faldi <oscarfaldi@gmail.com>
2026-05-26 02:27:28 +0700

    Create nvidia.nix

---
+++ nvidia.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    open = true;
+
+    modesetting.enable = true;
+
+    nvidiaSettings = true;
+  };
+}
