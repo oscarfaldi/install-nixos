714e054af917d0ac5136e5b628134457f5e7b332
Oscar Faldi <oscarfaldi@gmail.com>
2026-05-29 01:52:39 +0700

    Update mounts.nix

--- modules/core/mounts.nix
+++ modules/core/mounts.nix
@@ -1,88 +1,87 @@
 { config, pkgs, ... }:
 
 {
-  # Configuration SMB with automount (Prompt via GVFS/Thunar)
   fileSystems."/mnt/nas/academy" = {
-    device = "//10.10.20.10/academy";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/academy";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/appdata" = {
-    device = "//10.10.20.10/appdata";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/cache/appdata";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/archive" = {
-    device = "//10.10.20.10/archive";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/archive";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/download" = {
-    device = "//10.10.20.10/download";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/download";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/family" = {
-    device = "//10.10.20.10/family";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/family";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/games" = {
-    device = "//10.10.20.10/games";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/games";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/media" = {
-    device = "//10.10.20.10/media";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/media";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/mulino" = {
-    device = "//10.10.20.10/mulino";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/mulino";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/nutrivibes" = {
-    device = "//10.10.20.10/nutrivibes";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/nutrivibes";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/oscarfaldi" = {
-    device = "//10.10.20.10/oscarfaldi";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/oscarfaldi";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/personalbranding" = {
-    device = "//10.10.20.10/personalbranding";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/personalbranding";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/snapshot" = {
-    device = "//10.10.20.10/snapshot";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/snapshot";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/toolkit" = {
-    device = "//10.10.20.10/toolkit";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/toolkit";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 
   fileSystems."/mnt/nas/valhalla" = {
-    device = "//10.10.20.10/valhalla";
-    fsType = "cifs";
-    options = [ "x-systemd.automount" "noauto" "uid=1000" "gid=100" "file_mode=0755" "dir_mode=0755" ];
+    device = "10.10.20.10:/mnt/user/valhalla";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "x-systemd.automount" "noauto" ];
   };
 }
