92be5e8970d18f09a9a3d1c41f019e36ef03de5b
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-01 13:00:20 +0700

    Update nic.nix

--- modules/core/nic.nix
+++ modules/core/nic.nix
@@ -1,19 +1,51 @@
 { config, pkgs, ... }:
 
 {
-  # Intel X540-T2 10G direct NAS connection
+networking.networkmanager.enable = true;
 
-  networking.interfaces.enp4s0f0.ipv4.addresses = [
-    {
-      address = "10.10.20.11";
-      prefixLength = 24;
-    }
-  ];
+networking.networkmanager.ensureProfiles.profiles = {
+"Internet & NAS" = {
+connection = {
+id = "Internet & NAS";
+type = "ethernet";
+interface-name = "enp7s0";
+};
 
-  networking.interfaces.enp4s0f1.ipv4.addresses = [
-    {
-      address = "10.10.21.11";
-      prefixLength = 24;
-    }
-  ];
+```
+  ipv4.method = "auto";
+  ipv6.method = "auto";
+};
+
+"Intel X540-T2 10G Port 1" = {
+  connection = {
+    id = "Intel X540-T2 10G Port 1";
+    type = "ethernet";
+    interface-name = "enp4s0f0";
+  };
+
+  ipv4 = {
+    method = "manual";
+    address1 = "10.10.20.11/24";
+  };
+
+  ipv6.method = "disabled";
+};
+
+"Intel X540-T2 10G Port 2" = {
+  connection = {
+    id = "Intel X540-T2 10G Port 2";
+    type = "ethernet";
+    interface-name = "enp4s0f1";
+  };
+
+  ipv4 = {
+    method = "manual";
+    address1 = "10.10.21.11/24";
+  };
+
+  ipv6.method = "disabled";
+};
+```
+
+};
 }
