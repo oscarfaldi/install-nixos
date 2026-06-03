9d6bf9a81ec1554d69de50f59d2267b0f39bf893
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-03 23:30:01 +0700

    Update rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo " Rebuilding NixOS..."
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+
 sudo nixos-rebuild switch
+
+echo
+echo "✓ Rebuild complete."
