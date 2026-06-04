de12d0a32201bcc3d95eb3e8163bcfc49d5de814
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-04 15:49:54 +0700

    Delete theme.sh

--- theme.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-gsettings set org.gnome.desktop.interface gtk-theme Graphite-Dark
-gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
-gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Classic
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
