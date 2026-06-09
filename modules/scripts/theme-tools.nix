e6d34eadbf27084c08a2300af1a6c9e2a6be16b2
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-09 22:41:44 +0700

    Update theme-tools.nix

--- modules/scripts/theme-tools.nix
+++ modules/scripts/theme-tools.nix
@@ -1,19 +1,19 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 let
 
   mkScript = name:
     pkgs.writeShellScriptBin name
-      (builtins.readFile ../../assets/scripts/${name});
-
-  mkTheme = name: path:
-    pkgs.writeShellScriptBin name
-      (builtins.readFile path);
+      (builtins.readFile ../../modules/scripts/theme/${name});
 
 in
 {
-  environment.systemPackages = [
 
+  # ============================================================
+  # Environment Scripts (Themes & Wallpaper)
+  # ============================================================
+
+  environment.systemPackages = [
     (mkScript "theme-toggle")
     (mkScript "wallpaper")
     (mkScript "wallpaperd")
