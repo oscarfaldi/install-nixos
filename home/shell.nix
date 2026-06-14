f025224b8ce0b3cd6c9a850bf8e57586fe65c09b
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-14 15:09:22 +0700

    Update shell.nix

--- home/shell.nix
+++ home/shell.nix
@@ -1,4 +1,19 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
+  programs.zsh = {
+    enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  home.file.".zshrc".text = ''
+    source ~/.config/zshrc
+  '';
+
+  home.file.".bashrc".text = ''
+    source ~/.config/bashrc
+  '';
 }
