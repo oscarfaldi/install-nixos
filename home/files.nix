17e9fe1e5957ace4083b8d952e15d5d65e1a0418
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-14 15:09:47 +0700

    Update files.nix

--- home/files.nix
+++ home/files.nix
@@ -1,4 +1,30 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
+  home.file.".config/niri".source =
+    ../.config/niri;
+
+  home.file.".config/waybar".source =
+    ../.config/waybar;
+
+  home.file.".config/fuzzel".source =
+    ../.config/fuzzel;
+
+  home.file.".config/ghostty".source =
+    ../.config/ghostty;
+
+  home.file.".config/mako".source =
+    ../.config/mako;
+
+  home.file.".config/starship.toml".source =
+    ../.config/starship.toml;
+
+  home.file.".config/aliasrc".source =
+    ../.config/aliasrc;
+
+  home.file.".config/zshrc".source =
+    ../.config/zshrc;
+
+  home.file.".config/bashrc".source =
+    ../.config/bashrc;
 }
