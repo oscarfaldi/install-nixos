f99c24ac71ffeb0da8b153994873f8d5935ba095
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-14 15:18:17 +0700

    Update files.nix

--- home/files.nix
+++ home/files.nix
@@ -1,20 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  home.file.".config/niri".source =
-    ../.config/niri;
-
-  home.file.".config/waybar".source =
-    ../.config/waybar;
-
-  home.file.".config/fuzzel".source =
-    ../.config/fuzzel;
-
-  home.file.".config/ghostty".source =
-    ../.config/ghostty;
-
-  home.file.".config/mako".source =
-    ../.config/mako;
+  home.file.".config/niri".source = ../.config/niri;
+  home.file.".config/waybar".source = ../.config/waybar;
+  home.file.".config/fuzzel".source = ../.config/fuzzel;
+  home.file.".config/mako".source = ../.config/mako;
+  home.file.".config/ghostty".source = ../.config/ghostty;
+  home.file.".config/fastfetch".source = ../.config/fastfetch;
+  home.file.".config/mpv".source = ../.config/mpv;
+  home.file.".config/yazi".source = ../.config/yazi;
+  home.file.".config/qimgv".source = ../.config/qimgv;
+  home.file.".config/swayidle".source = ../.config/swayidle;
+  home.file.".config/swaylock".source = ../.config/swaylock;
+  home.file.".config/wlogout".source = ../.config/wlogout;
+  home.file.".config/xarchiver".source = ../.config/xarchiver;
 
   home.file.".config/starship.toml".source =
     ../.config/starship.toml;
@@ -22,9 +21,9 @@
   home.file.".config/aliasrc".source =
     ../.config/aliasrc;
 
-  home.file.".config/zshrc".source =
-    ../.config/zshrc;
-
   home.file.".config/bashrc".source =
     ../.config/bashrc;
+
+  home.file.".config/zshrc".source =
+    ../.config/zshrc;
 }
