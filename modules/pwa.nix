41341f661fefa38499cdc559d5562ae19c195119
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-15 22:08:53 +0700

    Update pwa.nix

--- modules/pwa.nix
+++ modules/pwa.nix
@@ -24,78 +24,77 @@ in
   };
 
   environment.systemPackages = with pkgs; [
-    chromium
 
     (mkPWA {
       name = "ChatGPT";
       url = "https://chatgpt.com";
-      icon = "~/.config/assets/icons/chatgpt.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/chatgpt.svg";
     })
 
     (mkPWA {
       name = "Canva";
       url = "https://canva.com";
-      icon = "~/.config/assets/icons/canva.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/canva.svg";
     })
 
     (mkPWA {
       name = "Gemini";
       url = "https://gemini.google.com";
-      icon = "~/.config/assets/icons/gemini.png";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/gemini.png";
     })
 
     (mkPWA {
       name = "Claude";
       url = "https://claude.ai";
-      icon = "~/.config/assets/icons/claude.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/claude.svg";
     })
 
     (mkPWA {
       name = "Unraid";
       url = "http://10.10.10.10:8081/";
-      icon = "~/.config/assets/icons/unraid.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/unraid.svg";
     })
 
     (mkPWA {
       name = "WhatsApp";
       url = "https://web.whatsapp.com";
-      icon = "~/.config/assets/icons/whatsapp.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/whatsapp.svg";
     })
 
     (mkPWA {
       name = "Wordpress";
       url = "https://oscarfaldi.com/wp-admin";
-      icon = "~/.config/assets/icons/wordpress.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/wordpress.svg";
     })
 
     (mkPWA {
       name = "Jellyfin";
       url = "http://10.10.10.10:8096/";
-      icon = "~/.config/assets/icons/jellyfin.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/jellyfin.svg";
     })
 
     (mkPWA {
       name = "Navidrome";
       url = "http://10.10.10.10:4533/";
-      icon = "~/.config/assets/icons/navidrome.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/navidrome.svg";
     })
 
     (mkPWA {
       name = "Obsidian";
       url = "http://10.10.10.10:3001/";
-      icon = "~/.config/assets/icons/obsidian.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/obsidian.svg";
     })
 
     (mkPWA {
       name = "Grafana";
       url = "http://10.10.10.10:3050/";
-      icon = "~/.config/assets/icons/grafana.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/grafana.svg";
     })
 
     (mkPWA {
       name = "Gmail";
       url = "https://mail.google.com";
-      icon = "~/.config/assets/icons/gmail.svg";
+      icon = "${config.home.homeDirectory}/.config/assets/icons/gmail.svg";
     })
   ];
 }
