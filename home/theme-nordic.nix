fa77d99ce2b067c274083fb10681651392218744
Oscar Faldi <oscarfaldi@gmail.com>
2026-06-18 00:39:28 +0700

    Update theme-nordic.nix

--- home/theme-nordic.nix
+++ home/theme-nordic.nix
@@ -4,11 +4,8 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-
       gtk-theme = "Nordic";
-
       icon-theme = "Papirus-Dark";
-
       cursor-theme = "Bibata-Modern-Ice";
     };
   };
